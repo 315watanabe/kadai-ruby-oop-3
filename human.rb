f05a3af931f388024f3e64d57d927325f0e0ee60
Ubuntu <ubuntu@ip-172-31-84-228.ec2.internal>
2023-02-16 07:30:54 +0000

    third commit

--- human.rb
+++ human.rb
@@ -1,10 +1,10 @@
+require './animal'
 require './thinkable'
 class Human < Animal
   include Thinkable
   attr_accessor :hobby
   def initialize(name, age, hobby)
-    self.name = name
-    self.age = age
+    super(name, age)
     self.hobby = hobby
   end
 end
